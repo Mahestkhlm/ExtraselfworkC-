7daa40ad20096fc157b36241bba78553b7375573
Mahe <mahe22mca@gmail.com>
2020-03-19 10:36:22 +0100

    Add project files.

---
+++ Triangle/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Triangle
+{
+    using System;
+    public class Exercise14
+    {
+        public static void Main()
+        {
+            int SideX, SideY, SideZ;
+            Console.Write("\n\n");
+            Console.Write("Check If the triangle is Equilateral, Isosceles or Scalene:\n");
+            Console.Write("----------------------------------------------------------------");
+            Console.Write("\n\n");
+
+
+
+            Console.Write("Enter side A of triangle: ");
+            SideX  = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter side B of triangle: ");
+            SideY = int.Parse (Console.ReadLine());
+
+            Console.Write("Enter side C of triangle: ");
+            SideZ = int.Parse (Console.ReadLine());
+
+
+
+            if (SideX == SideY && SideY == SideZ)
+            {
+                Console.Write("This is an Equilateral triangle.\n");
+            }
+            else if (SideX == SideY || SideX == SideZ  || SideY == SideZ)
+            {
+                Console.Write("This is an Isosceles triangle.\n");
+            }
+            else
+            {
+                Console.Write("This is a Scalene triangle.\n");
+            }
+        }
+    }
+
+}
